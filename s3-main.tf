4396dd3933ec413cd8fb0e57953628538903a668
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-12-09 11:06:00 +0100

    terraform remote state

--- s3-main.tf
+++ s3-main.tf
@@ -7,8 +7,8 @@ terraform {
   required_providers { aws = ">=2.40.0" }
   required_version = ">=0.12.8"
   backend "s3" {
-    bucket = "tsb-terraform-remote-state-prod"
-    key    = "bezirksregioneneprofile-daten/terraform.tfstate.d/terraform.tfstate"
-        region = "eu-central-1"
+    bucket  = "tsb-terraform-remote-state-prod"
+    key     = "bezirksregioneneprofile-daten/terraform.tfstate.d/terraform.tfstate"
+    region  = "eu-central-1"
   }
 }
--- s3-variables.tf
+++ s3-variables.tf
@@ -2,6 +2,7 @@ variable "region" {
   default = "eu-central-1"
 }
 variable "profile" {
+  default = "tsberlin"
 }
 variable "name" {
   default = "data"
